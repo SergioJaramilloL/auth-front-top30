a0ec3bf2056d852bf2ebcb5df4836858258a15e3
SergioJaramilloL <sergiojaramilloluna@gmail.com>
2023-08-15 16:38:25 -0500

    First commit

---
+++ src/router/index.jsx
@@ -0,0 +1,29 @@
+import {createBrowserRouter} from 'react-router-dom'
+
+import Root from '../layout/Root'
+import Home from '../pages/Home'
+import NotFound from '../pages/NotFound'
+import Profile from '../pages/Profile'
+import PrivateRoute from '../components/PrivateRoute'
+
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: '/profile',
+        element: <PrivateRoute> <Profile /> </PrivateRoute>
+      }
+    ]
+  },
+])
+
+export default router
